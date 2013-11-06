2ccad560649d34a60fab067f5654122120eacd90
RaVhen <raven-19@gmx.fr>
2013-11-06 10:38:33 +0100

    Redone to read file

--- Lucas/fenetre.c
+++ Lucas/fenetre.c
@@ -1,10 +1,13 @@
 // pour compiler : gcc -Wall -g -lm fenetre.c -lcsfml-graphics  -o fenetre
 //
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <SFML/Graphics.h> 
 
 #define MASS 1
+#define GRAVITY 1
+#define SPRING 7
 
 typedef struct Node_{            // Node struct
   float posX, posY;
@@ -61,14 +64,14 @@ Link createLink(Link l, Node *nA, Node *nB){
 
 int genesis(Node **nodes, Link *links, char* filename, int* nodeCt)
 {
-  FILE * inputFile;
+  /*FILE * inputFile;
   inputFile = fopen(filename, "r");
   printf("Try to open %s\n", filename);
   
   if(inputFile==NULL)
     return(1); //error reading file !
-  
-  fscanf(inputFile, "%d", nodeCt); //get nodes count
+  */
+  scanf("%d", nodeCt); //get nodes count
   *nodes = malloc(sizeof(Node)*(*nodeCt)); //create nodes array
   
   int i;
@@ -78,16 +81,36 @@ int genesis(Node **nodes, Link *links, char* filename, int* nodeCt)
   }
   
   int nodeA, nodeB;
-  while(fscanf(inputFile, "%d-%d", &nodeA, &nodeB) == 2)
+  while(scanf("%d-%d", &nodeA, &nodeB) == 2)
   {
     //DEBUG: printf("NoeudA:%d  NoeudB:%d\n", nodeA, nodeB);
     *links = createLink(*links, &((*nodes)[nodeA]), &((*nodes)[nodeB])); //create a link between the 2 specified nodes.
   }
   
-  fclose(inputFile);
+  //fclose(inputFile);
   return 0;
 }
 
+void updateForces(Node *nodes, int nodeCt){
+  float resultX = 0;
+  float resultY = 0;
+  int current = 0, neighbour = 0;
+  for(current = 0; current < nodeCt-1; current++){
+    for(neighbour = current+1; neighbour < nodeCt; neighbour++){
+      // REINISTAILISER LES FORCESX & Y
+      float disX = nodes[current].posX - nodes[neighbour].posX;
+      float disY = nodes[current].posY - nodes[neighbour].posY;
+      float dis = (disX*disX) +(disY*disY);
+      float resultXtemp = 0;
+      if(disX!=0)
+	resultXtemp =  GRAVITY*(MASS*MASS/(dis))*(abs(disX)/sqrt(dis));
+      if(disX<0) //if current is at the left of neighbour
+	nodes[current].posX -= resultXtemp;
+      if(disX>0)
+	resultX+= resultXtemp;
+    }
+  }
+}
 
 void simulation(float deltaT, float *x, float *y) {
   float vitesse = 10; // unité : pixel/secondes
@@ -99,7 +122,7 @@ void simulation(float deltaT, float *x, float *y) {
 void affichage(sfRenderWindow *fenetre, Node *nodes, int nodeCt, Link links) {
   // préparation des couleurs
   sfColor noir = {0,0,0,0}, blanc = {255,255,255,255}, gris = {128,128,128,128};
-  
+
   sfShape **cercles = malloc(sizeof(sfShape*)*nodeCt);
   int i;
   for(i=0;i<nodeCt;i++){//creer les cercles 
@@ -113,7 +136,8 @@ void affichage(sfRenderWindow *fenetre, Node *nodes, int nodeCt, Link links) {
     linkCt++;
     tmp = tmp->next;
   }
-  printf("linkcT = %d\n",linkCt);
+  //Debug to print links number
+  //printf("linkcT = %d\n",linkCt);
   sfShape ** lignes = malloc(sizeof(sfShape*)*linkCt);
   for(i=0;i<linkCt;i++){
     lignes[i] = sfShape_CreateLine(links->nodeA->posX,links->nodeA->posY,links->nodeB->posX,links->nodeB->posY,1,blanc,1,gris); 
@@ -121,41 +145,35 @@ void affichage(sfRenderWindow *fenetre, Node *nodes, int nodeCt, Link links) {
   }
 
   // préparation du texte à afficher
-  
   sfString **texte = malloc(sizeof(sfString*)*nodeCt);
-  char s[10];
+  char *s = malloc(sizeof(char)*20);
   for(i=0;i<nodeCt;i++){
-    texte[i] = sfString_Create();
+    texte[i] = sfString_Create(); // seems to create a reading memory eroor in valgrind, cant find documentation on it :/
     sprintf(s,"%d",i);
     sfString_SetText(texte[i],s);
     sfString_SetPosition(texte[i],nodes[i].posX +10, nodes[i].posY+10);
   }
   sfRenderWindow_Clear(fenetre,noir);// effacer l'écran  
 
-  //sfRenderWindow_DrawShape(fenetre,cercle);  // tracer le cercle
   for(i=0;i<nodeCt;i++){//tracer les cercles
     sfRenderWindow_DrawShape(fenetre,cercles[i]);
-  }
-  for(i=0;i<linkCt;i++){
-    sfRenderWindow_DrawShape(fenetre,lignes[i]);
-  }    // tracer la ligne
-  for(i=0;i<nodeCt;i++){//tracer les textes
     sfRenderWindow_DrawString(fenetre,texte[i]);
-  } 
+  }
+  for(i=0;i<linkCt;i++) // Tracer des lignes
+    sfRenderWindow_DrawShape(fenetre,lignes[i]);
   sfRenderWindow_Display(fenetre); // visualiser le contenu de la fenetre    
 
+  free(s);
   // faire le ménage en mémoire
-
   for(i=0;i<linkCt;i++)
     sfShape_Destroy(lignes[i]);
-    
   for(i=0;i<nodeCt;i++){
     sfShape_Destroy(cercles[i]);
     sfString_Destroy(texte[i]);
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
     // initialisation graphique
     sfRenderWindow *fenetre;
     sfVideoMode mode = {640, 480, 32};
@@ -169,9 +187,13 @@ int main() {
     deltaT = 0.01;// doit etre fixe sinon l'algo dépend de vitesse de la machine
                   // d'exécution !! (PB de correction, surtout avec valgrind!) 
                   // Ne modifiez donc pas cette valeur!
-    
-    char* testFile = "graphe.txt";
-    
+    char* testFile = malloc(sizeof(char*)*70);
+    if(argc > 1)
+      testFile = argv[1];
+    else{
+      //testFile = "graphe.txt";
+    }
+    printf("%s\n",testFile);
     Node *nodes = NULL;
     Link links = NULL;
     int nodeCt = 0;
@@ -184,7 +206,7 @@ int main() {
     int i = 0;
     for(i = 0;i < nodeCt; i++){
       
-      printf("Premier noeud: %.1f %.1f\n", nodes[i].posX, nodes[i].posY);
+      printf("Node #%d: %.1f %.1f\n", i,nodes[i].posX, nodes[i].posY);
     }
     
     
